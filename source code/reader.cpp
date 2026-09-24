0dc6eeedbaecd3993ce1bb757d6b85331d96e033
corrupt_u <goog@goog.co>
2026-09-24 18:38:03 -0500

    fuck 4

--- source code/reader.cpp
+++ source code/reader.cpp
@@ -56,4 +56,6 @@ int assignSubject(varLine) {
     // get the subject name of the subject variable, and attach it to the data type listed in tokens. Furthermore, attach nested modifiers and also the data assigned to the subject.
 
     // Where to start
+
+    //
 }
