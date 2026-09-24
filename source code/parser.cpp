696416fba466114dca2ce113b84b63ed7f9f5b98
corrupt_u <goog@goog.co>
2026-09-23 21:54:16 -0500

    shit

--- source code/parser.cpp
+++ source code/parser.cpp
@@ -1,1 +1,4 @@
 #include <backend.cpp>
+#include <reader.cpp>
+
+// This file is the main parser for the flow-spsl language. It takes in a list of tokens and parses them into an abstract syntax tree (AST). The AST is then used to generate code for the target platform.
